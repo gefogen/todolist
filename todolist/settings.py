6dabf6b09c78a8a3a7db6f407b8dcbc7d2e04902
GeFoGen <gefogen>
2022-12-07 22:21:38 +0300

    init

--- todolist/settings.py
+++ todolist/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
