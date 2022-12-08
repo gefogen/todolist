7151bbedf5bf33609b6ebf2a3405ae744ae64eb0
GeFoGen <gefogen>
2022-12-08 10:59:36 +0300

    init

--- todolist/settings.py
+++ todolist/settings.py
@@ -80,16 +80,7 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+DATABASES = {"default": env.db()}
 
 
 # Password validation
