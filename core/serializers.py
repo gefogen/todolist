07028afa35c1f52b89781b35146258ca49e8feae
GeFoGen <gefogen>
2022-12-17 10:54:28 +0300

    add API registr

---
+++ core/serializers.py
@@ -0,0 +1,63 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from core.models import User
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password_repeat = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        read_only_fields = ("id",)
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "password_repeat",
+        )
+
+    def validate(self, attrs: dict):
+        password: str = attrs.get("password")
+        password_repeat: str = attrs.pop("password_repeat", None)
+        if password != password_repeat:
+            raise ValidationError("password and password_repeat is not equal")
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        self.user = user
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs: dict):
+        username = attrs.get("username")
+        password = attrs.get("password")
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise ValidationError("username or password is incorrect")
+        attrs["user"] = user
+        return attrs
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        read_only_fields = ("id",)
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+        )
