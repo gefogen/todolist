148a29b637668e0ce66f49410d071abd77030edf
GeFoGen <gefogen>
2022-12-18 00:34:38 +0300

    test

--- todolist/settings.py
+++ todolist/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     "rest_framework",
-    'social_django',
+    # 'social_django',
 ]
 
 MIDDLEWARE = [
@@ -119,29 +119,29 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = 'core.User'
 
 AUTHENTICATION_BACKENDS = (
-    "social_core.backends.vk.VKOAuth2",
+    # "social_core.backends.vk.VKOAuth2",
     "django.contrib.auth.backends.ModelBackend",
 )
 
 # Social
-SOCIAL_AUTH_JSONFIELD_ENABLED = True
-SOCIAL_AUTH_URL_NAMESPACE = "social"
-SOCIAL_AUTH_PIPELINE = (
-    "social_core.pipeline.social_auth.social_details",
-    "social_core.pipeline.social_auth.social_uid",
-    "social_core.pipeline.social_auth.social_user",
-    "social_core.pipeline.user.get_username",
-    "social_core.pipeline.social_auth.associate_by_email",
-    "social_core.pipeline.user.create_user",
-    "social_core.pipeline.social_auth.associate_user",
-    "social_core.pipeline.social_auth.load_extra_data",
-    "social_core.pipeline.user.user_details",
-)
-SOCIAL_AUTH_VK_OAUTH2_KEY = env.str("SOCIAL_AUTH_VK_OAUTH2_KEY")
-SOCIAL_AUTH_VK_OAUTH2_SECRET = env.str("SOCIAL_AUTH_VK_OAUTH2_SECRET")
-SOCIAL_AUTH_VK_OAUTH2_SCOPE = ["email", "photos", "notify"]
-SOCIAL_AUTH_LOGIN_REDIRECT_URL = "/logged-in/"
-SOCIAL_AUTH_LOGIN_ERROR_URL = "/login-error/"
+# SOCIAL_AUTH_JSONFIELD_ENABLED = True
+# SOCIAL_AUTH_URL_NAMESPACE = "social"
+# SOCIAL_AUTH_PIPELINE = (
+#     "social_core.pipeline.social_auth.social_details",
+#     "social_core.pipeline.social_auth.social_uid",
+#     "social_core.pipeline.social_auth.social_user",
+#     "social_core.pipeline.user.get_username",
+#     "social_core.pipeline.social_auth.associate_by_email",
+#     "social_core.pipeline.user.create_user",
+#     "social_core.pipeline.social_auth.associate_user",
+#     "social_core.pipeline.social_auth.load_extra_data",
+#     "social_core.pipeline.user.user_details",
+# )
+# SOCIAL_AUTH_VK_OAUTH2_KEY = env.str("SOCIAL_AUTH_VK_OAUTH2_KEY")
+# SOCIAL_AUTH_VK_OAUTH2_SECRET = env.str("SOCIAL_AUTH_VK_OAUTH2_SECRET")
+# SOCIAL_AUTH_VK_OAUTH2_SCOPE = ["email", "photos", "notify"]
+# SOCIAL_AUTH_LOGIN_REDIRECT_URL = "/logged-in/"
+# SOCIAL_AUTH_LOGIN_ERROR_URL = "/login-error/"
 
 
 # Internationalization
--- todolist/urls.py
+++ todolist/urls.py
@@ -17,6 +17,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('oauth/', include('social_django.urls', namespace='social')),
+    # path('oauth/', include('social_django.urls', namespace='social')),
     path('core/', include('core.urls'))
 ]
