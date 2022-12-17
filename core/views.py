c04198d2d725c6187063d40dac827cf952a5a73e
GeFoGen <gefogen>
2022-12-17 10:58:56 +0300

    add some for API registr

--- core/views.py
+++ core/views.py
@@ -1,3 +1,34 @@
+from django.contrib.auth import login
 from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from core.models import User
+from core.serializers import CreateUserSerializer, LoginSerializer, UserSerializer
+
+
+class SignupView(CreateAPIView):
+    model = User
+    permission_classes = [permissions.AllowAny]
+    serializer_class = CreateUserSerializer
+
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        login(
+            self.request,
+            user=serializer.user,
+            backend="django.contrib.auth.backends.ModelBackend",
+        )
+
+
+class LoginView(GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        s: LoginSerializer = self.get_serializer(data=request.data)
+        s.is_valid(raise_exception=True)
+        user = s.validated_data["user"]
+        login(request, user=user)
+        user_serializer = UserSerializer(instance=user)
+        return Response(user_serializer.data)
