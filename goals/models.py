39bf7d88b4aef39956f1207b9b921d90e84b5f06
GeFoGen <gefogen>
2022-12-26 19:15:13 +0300

    bug fixes

--- goals/models.py
+++ goals/models.py
@@ -6,13 +6,8 @@ from core.models import User
 
 class DatesModelMixin(models.Model):
     class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
         abstract = True
 
-    title = models.CharField(verbose_name="Название", max_length=255)
-    user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
-    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
     created = models.DateTimeField(verbose_name="Дата создания")
     updated = models.DateTimeField(verbose_name="Дата последнего обновления")
 
